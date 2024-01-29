47d04dda9d6a95b9c6d0829b41b52c4c20973db1
user <user@XVIII-DEMS.local>
2024-01-29 20:07:02 +0100

    added undraw icon for empty data

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -2,6 +2,7 @@ import OrderCard from "./OrderCard";
 import { data } from "../mockData";
 import { useRef, useState } from "react";
 import { animateScroll } from "react-scroll";
+import emptyIcon from "../assets/images/no-data.svg";
 const Orders = () => {
   const ordersRef = useRef(null);
 
@@ -25,35 +26,67 @@ const Orders = () => {
 
   return (
     <div ref={ordersRef} id="list" className="orders">
-      <div className="top-header">
-        <h3>Last Orders</h3>
-        <button
-          className={disableBtn ? "disable" : "top-header-btn"}
-          disabled={disableBtn}
-          onClick={toggleDisplay}
-        >
-          {expanded ? "See Less" : "See All"}{" "}
-        </button>
-      </div>
-      <div className="ordercard-wrapper-div">
-        <div className="order-header">
-          <p>Name</p>
-          <p>Date</p>
-          <p>Amount</p>
-          <p>Status</p>
-          <p>Invoice</p>
-        </div>
+      {data.length !== 0 ? (
+        <>
+          <div className="top-header">
+            <h3>Last Orders</h3>
+            <button
+              className={disableBtn ? "disable" : "top-header-btn"}
+              disabled={disableBtn}
+              onClick={toggleDisplay}
+            >
+              {expanded ? "See Less" : "See All"}{" "}
+            </button>
+          </div>
+          <div className="ordercard-wrapper-div">
+            <div className="order-header">
+              <p>Name</p>
+              <p>Date</p>
+              <p>Amount</p>
+              <p>Status</p>
+              <p>Invoice</p>
+            </div>
 
-        {dataToShow.map((item) => (
-          <OrderCard
-            name={item.name}
-            date={item.date}
-            amount={item.amount}
-            status={item.status}
-            profile={item.profile}
+            {dataToShow.map((item) => (
+              <OrderCard
+                name={item.name}
+                date={item.date}
+                amount={item.amount}
+                status={item.status}
+                profile={item.profile}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <p
+            style={{
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: "32px",
+              marginBottom: "1.7rem",
+            }}
+          >
+            No Orders to Show
+          </p>
+          <img
+            style={{
+              width: "240px",
+              height: "240px",
+              display: "block",
+              margin: "0 auto",
+            }}
+            src={emptyIcon}
+            alt="emptyIcon"
           />
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
